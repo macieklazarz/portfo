7a90bb310da4a646b1983637952eb1cdd12f0255
unknown <macieklazarz@gmail.com>
2021-04-20 14:30:37 +0200

    20210420

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for, request, redirect
+from email.message import EmailMessage
+from twilio.rest import Client
 import csv
 import datetime
+import smtplib
+import time
 app = Flask(__name__)
 # print(__name__)
 
@@ -17,24 +21,77 @@ def write_to_csv(data):
         csv_writer = csv.writer(database2, delimiter=',',quotechar='"',  quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([mail,subject,message,timestamp])
 
+def mail_sender(data):
+    email = EmailMessage()
+    email['from'] = data.get("mail")
+    email['to'] = 'macieklazarz@gmail.com'
+    email['subject'] = data.get("subject")
+
+    email.set_content(data.get("message"))
+
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login('92maciek92maciek92@gmail.com', 'c1helsea')
+        smtp.send_message(email)
+        print('all good')
+
+
+def sms_message():
+    account_sid = 'AC3172e7ab734af3e69c3d7ad2fea4d486'
+    auth_token = '9939169f739b335f66bf4e34af358b81'
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        to="+48726417085",
+        from_="+12058435485",
+        body="you have new message on mail")
+
+    print(message.sid)
+
+
+def time_stamp():
+    with open('time_file.txt', mode='w') as time_file:
+        time_file.write(str(time.time()))
+
+
+def time_compare():
+    with open('time_file.txt', mode='r') as time_file:
+        var1 = float(time_file.read())
+        print(var1)
+        print(time.time()-var1)
+        return(time.time()-var1)
+
 
 @app.route('/')
 def my_home():
     # print(url_for('static', filename='bolt.ico'))
     return render_template('index.html')
 
+
 @app.route('/<name>')
 def my_home2(name=None):
     # print(url_for('static', filename='bolt.ico'))
     return render_template(name)
 
+
 @app.route('/submit_form', methods = ['POST','GET'])
 def submit_form():
     if request.method=='POST':
         data = request.form.to_dict()
-        print(data)
-        write_to_file(data)
-        write_to_csv(data)
-        return redirect('/thankyou.html')
+        # print(data)
+        # print(data.get("mail"))
+
+        if time_compare()>200:
+            # print("ok!")
+            write_to_file(data)
+            write_to_csv(data)
+            time_stamp()
+            # mail_sender(data)
+            # sms_message()
+            return redirect('/thankyou.html')
+        else:
+            # print("lipa")
+            return redirect('/toomanymessages.html')
     else:
         return 'sth is wrong'
