6aae8dd19959a46d7b86dfd0c705aa38fe669c64
unknown <macieklazarz@gmail.com>
2021-04-24 20:41:05 +0200

    20210424

--- server.py
+++ server.py
@@ -27,19 +27,19 @@ def mail_sender(data):
     email['to'] = 'macieklazarz@gmail.com'
     email['subject'] = data.get("subject")
 
-    email.set_content(data.get("message"))
+    email.set_content(f'Message from {data.get("mail")}: {data.get("message")}')
 
     with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
         smtp.ehlo()
         smtp.starttls()
         smtp.login('92maciek92maciek92@gmail.com', 'c1helsea')
         smtp.send_message(email)
-        print('all good')
+        # print(email)
 
 
 def sms_message():
     account_sid = 'AC3172e7ab734af3e69c3d7ad2fea4d486'
-    auth_token = '9939169f739b335f66bf4e34af358b81'
+    auth_token = 'f3b44fca0081f9ed3b027f7e59f74bb2'
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
@@ -50,6 +50,10 @@ def sms_message():
     print(message.sid)
 
 
+def not_sent(message):
+    with open('mail_sms_failure.txt', mode='a') as mail_sms_failure:
+        mail_sms_failure.write(f'\n{datetime.datetime.now()} - {message}')
+
 def time_stamp():
     with open('time_file.txt', mode='w') as time_file:
         time_file.write(str(time.time()))
@@ -58,8 +62,8 @@ def time_stamp():
 def time_compare():
     with open('time_file.txt', mode='r') as time_file:
         var1 = float(time_file.read())
-        print(var1)
-        print(time.time()-var1)
+        # print(f'poprzedni czas: {var1}')
+        # print(f'roznica czasow{time.time()-var1}')
         return(time.time()-var1)
 
 
@@ -87,8 +91,14 @@ def submit_form():
             write_to_file(data)
             write_to_csv(data)
             time_stamp()
-            # mail_sender(data)
-            # sms_message()
+            try:
+                mail_sender(data)
+            except:
+               not_sent('mail not sent')
+            try:
+                sms_message()
+            except:
+                not_sent('sms not sent')
             return redirect('/thankyou.html')
         else:
             # print("lipa")
